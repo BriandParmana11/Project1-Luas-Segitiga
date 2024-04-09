530c6f081042eda876459703b6c307144435e2db
BriandParmana11 <117556443+BriandParmana11@users.noreply.github.com>
2024-04-09 19:33:16 +0800

    Add files via upload

---
+++ Project1.java
@@ -0,0 +1,30 @@
+package project1;
+
+/**
+ *
+ * @author briandparmana
+ * TGL: 2024-03-28
+ */
+
+import java.util.Scanner;
+
+public class Project1 {
+
+
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+
+        // Meminta input alas dan tinggi dari user
+        System.out.print("Masukkan nilai alas segitiga: ");
+        double alas = scanner.nextDouble();
+        System.out.print("Masukkan nilai tinggi segitiga: ");
+        double tinggi = scanner.nextDouble();
+
+        // Menghitung luas segitiga
+        double luas = 0.5 * alas * tinggi;
+
+        // Menampilkan hasil luas segitiga
+        System.out.println("Luas segitiga dengan alas " + alas + " dan tinggi " + tinggi + " adalah " + luas);
+    }
+    
+}
